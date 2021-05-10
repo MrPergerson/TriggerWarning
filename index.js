777fc98763d32e31752a869cf2f5ec1cc14bb638
Cole Pergerson <cole.pergerson@gmail.com>
2021-05-10 15:18:19 -0700

    updated style

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ $("#cover1-btn").click(() => {
 $("#cover2-btn").click(() => {
     $("#cover2").addClass( "invisible" );
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
